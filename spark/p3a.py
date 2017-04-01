e754d8ca189755be64889dfd045c911718c3aca3
Danny Villanueva Vega <danny.villanueva1@upr.edu>
2017-04-01 17:00:09 -0400

    Pregunta 3

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,6 @@
 #Code here
+import csv
+dataStudents = sc.textFile("/home/danny/studentsPR.csv")
+listaStudent=dataStudents.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[6])
+Students=listaStudent.filter(lambda x: x[5] == 'F').filter(lambda x: x[2] == '71381')
+Students.foreach(print)
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,9 @@
 #Code here
+import csv
+dataEscuela = sc.textFile("/home/carlos/Files/escuelasPR.csv")
+listaEscuela=dataEscuela.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[3])
+ponce = listaEscuela.filter(lambda x: x[2] == 'Ponce')
+caboRojo = listaEscuela.filter(lambda x: x[2] == 'Cabo Rojo')
+dorado = listaEscuela.filter(lambda x: x[2] == 'Dorado')
+Escuelas = ponce.union(caboRojo).union(dorado)
+Escuelas.foreach(print)
